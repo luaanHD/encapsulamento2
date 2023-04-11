2cf6665ee6d69e06f7726b33d847c89780106c2e
luaanHD <luantoniolo94@gmail.com>
2023-04-11 13:31:32 +0000

    Exercício 2 de encapsulamento

---
+++ Pessoa.java
@@ -0,0 +1,36 @@
+public class Pessoa {
+    
+    private String nome;
+    private int idade;
+    private String celular;
+    private String cpf;
+
+    //métodos de acesso público
+    public String getNome(){
+        return this.nome;
+    }
+    public void setNome(String nome){
+        this.nome=nome;
+    }
+
+    public int getIdade(){
+        return this.idade;
+    }
+    public void setIdade(int idade){
+        this.idade=idade;
+    }
+
+    public String getCelular(){
+        return this.celular;
+    }
+    public void setCelular(String celular){
+        this.celular=celular;
+    }
+
+    public String getCpf(){
+        return this.cpf;
+    }
+    public void setCpf(String cpf){
+        this.cpf=cpf;
+    }
+}
